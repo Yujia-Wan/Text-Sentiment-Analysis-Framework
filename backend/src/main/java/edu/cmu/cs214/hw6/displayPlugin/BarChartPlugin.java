6cb6586169723f0aecb92804353da02c78680150
junyilu <junyilu@andrew.cmu.edu>
2022-04-17 13:14:44 -0400

    implemented BarChartPlugin with its test

--- backend/src/main/java/edu/cmu/cs214/hw6/displayPlugin/BarChartPlugin.java
+++ backend/src/main/java/edu/cmu/cs214/hw6/displayPlugin/BarChartPlugin.java
@@ -1,4 +1,51 @@
 package edu.cmu.cs214.hw6.displayPlugin;
 
-public class BarChartPlugin {
+import edu.cmu.cs214.hw6.dataPlugin.Data;
+import edu.cmu.cs214.hw6.framework.core.DisplayPlugin;
+import edu.cmu.cs214.hw6.framework.core.Framework;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.*;
+
+public class BarChartPlugin implements DisplayPlugin {
+    private static final String DISPLAY_PLUGIN_NAME = "bar";
+    private Framework framework;
+
+    @Override
+    public String getDisplayPluginName() {
+        return DISPLAY_PLUGIN_NAME;
+    }
+
+    @Override
+    public JSONObject getRetrievedData(List<Data> list) {
+        JSONObject json = new JSONObject();
+        if (list.size() == 0) {
+            json.put("errorMessage", "No data to plot!");
+        }
+        Map<String, Integer> number = new HashMap<>();
+        Map<String, Float> score = new HashMap<>();
+        for (Data data : list) {
+            number.put(data.getTime(), number.getOrDefault(data.getTime(), 0) + 1);
+            score.put(data.getTime(), score.getOrDefault(data.getTime(), (float)0.0) + data.getScore());
+        }
+        List<String> x = new ArrayList<>();
+        List<Float> y = new ArrayList<>();
+        for (String time : number.keySet()) {
+            x.add(time);
+            y.add(score.get(time)/number.get(time));
+        }
+        json.put("x", new JSONArray(x));
+        json.put("y", new JSONArray(y));
+        JSONArray array = new JSONArray();
+        array.put(json);
+        JSONObject output = new JSONObject();
+        output.put("data", array);
+        return output;
+    }
+
+    @Override
+    public void onRegister(Framework framework) {
+        this.framework = framework;
+    }
 }
---
+++ backend/src/test/java/edu/cmu/cs214/hw6/BarChartTest.java
@@ -0,0 +1,33 @@
+package edu.cmu.cs214.hw6;
+
+import edu.cmu.cs214.hw6.dataPlugin.Data;
+import edu.cmu.cs214.hw6.displayPlugin.BarChartPlugin;
+import edu.cmu.cs214.hw6.framework.core.DisplayPlugin;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarChartTest {
+    private BarChartPlugin barChartPlugin;
+
+    @Before
+    public void setUp() {
+        barChartPlugin = new BarChartPlugin();
+    }
+
+    @Test
+    public void testGetRetrievedData() {
+        List<Data> input = new ArrayList<>();
+        Data data1 = new Data("","2022-04-16", (float) 0.8);
+        Data data2 = new Data("","2022-04-16", (float) 1);
+        Data data3 = new Data("","2022-04-15", (float) 0.7);
+        input.add(data1);
+        input.add(data2);
+        input.add(data3);
+        DisplayPlugin bar = new BarChartPlugin();
+        JSONObject output = bar.getRetrievedData(input);
+        System.out.println(output);
+    }
+}
